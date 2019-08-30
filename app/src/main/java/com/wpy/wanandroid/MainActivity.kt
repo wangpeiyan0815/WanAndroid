1b5b35baf0680bcae8936a046eee84190a528c35
wpy <18514431885@163.com>
2019-08-30 23:16:54 +0800

    测试提交

--- app/src/main/java/com/wpy/wanandroid/MainActivity.kt
+++ app/src/main/java/com/wpy/wanandroid/MainActivity.kt
@@ -3,6 +3,9 @@ package com.wpy.wanandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 测试提交
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
