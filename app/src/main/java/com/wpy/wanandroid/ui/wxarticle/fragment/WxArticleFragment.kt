04c8397cc29ceb629ba3fb7255b6b9b96267a48a
wpy <18514431885@163.com>
2019-09-13 09:02:44 +0800

    添加cookie 拦截器

--- app/src/main/java/com/wpy/wanandroid/ui/wxarticle/fragment/WxArticleFragment.kt
+++ app/src/main/java/com/wpy/wanandroid/ui/wxarticle/fragment/WxArticleFragment.kt
@@ -13,7 +13,6 @@ import com.wpy.wanandroid.ui.wxarticle.presenter.WxArticlePresenterImpl
 import com.wpy.wanandroid.utils.SmartRefreshUtils
 import kotlinx.android.synthetic.main.fragment_wx_article.*
 
-
 class WxArticleFragment : BaseMvpFragment<WxArticlePresenterImpl>(), WxArticleContract.View {
 
     private lateinit var mAdapter: WxArticleAdapter
